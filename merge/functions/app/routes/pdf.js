a83f7c877eda71d743f1cb99d2cfa04a3e7aa739
Iván Arango <73449144+IvanArango98@users.noreply.github.com>
2022-02-03 10:38:09 -0600

    fixing errors

--- merge/functions/app/routes/pdf.js
+++ merge/functions/app/routes/pdf.js
@@ -1,54 +1,46 @@
-  
-const express = require("express")
-const router = express.Router()
-const {PDFDocument} = require('pdf-lib')
-const fetch = require('node-fetch')
+const express = require("express");
+const router = express.Router();
+const { PDFDocument } = require("pdf-lib");
+const fetch = require("node-fetch");
 
-router.use(express.json())
+router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
-router.get("/", async function(req,res) {
-  var datas = req.query.data;
-  res.status(200).json({message: datas});
-  if(!datas){
-    res.status(429).json({message: "you must add an url"});
-  }
-  if(!Array.isArray(datas)){
-    datas = [datas];
-  }
-     
-    // Load cover and content pdfs
-    var files = []
-  const url = `${datas[0]}`
-  for(var i =0; i < datas.length;i++)
-  {
-    const url = `${datas[i]}`
-    const PdfBytes = await fetch(url).then(res => res.arrayBuffer())
+router.get("/", async function (req, res) {
+  var datas = req.query.data;  
+
+  // Load cover and content pdfs
+  var files = [];
+  const url = `${datas[0]}`;
+  for (var i = 0; i < datas.length; i++) {
+    const url = `${datas[i]}`;
+    const PdfBytes = await fetch(url).then((res) => res.arrayBuffer());
     const cover = await PDFDocument.load(PdfBytes);
-    files.push(cover)
+    files.push(cover);
   }
 
   // Create a new document
   const doc = await PDFDocument.create();
 
   // Add individual content pages
-  for(var i=0; i < files.length;i++)
-  {
-    const contentPages = await doc.copyPages(files[i], files[i].getPageIndices());
+  for (var i = 0; i < files.length; i++) {
+    const contentPages = await doc.copyPages(
+      files[i],
+      files[i].getPageIndices()
+    );
     for (const page of contentPages) {
       doc.addPage(page);
-    }  
+    }
   }
-  let data1 =  await doc.save();  
+  let data1 = await doc.save();
   let binary = await data1;
   res.writeHead(200, {
-    'Content-Type': "application/pdf",
-    'Content-disposition': 'attachment;filename=documentacion.pdf',
-    'Content-Length': binary.length
+    "Content-Type": "application/pdf",
+    "Content-disposition": `attachment;filename=${req.query.nombre}.pdf`,
+    "Content-Length": binary.length,
   });
 
-  res.end(Buffer.from(binary, 'binary'));
-
-})
+  res.end(Buffer.from(binary, "binary"));
+});
 
 module.exports = router;
